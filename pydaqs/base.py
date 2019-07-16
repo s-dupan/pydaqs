c056a6fa7e53c9ffa1048c0a1d359368984e2050
agamemnonc <agamemnon.krasoulis@gmail.com>
2019-07-16 15:10:44 +0100

    base

---
+++ pydaqs/base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class _BaseDAQ(ABC):
+    """
+    Base class for DAQ stream reading devices.
+
+    Warning: This class should not be used directly.
+    Use derived classes instead.
+    """
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
